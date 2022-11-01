1e8ba72b3246f8e3dff17c75f92ad49d8967dc93
zxxzyyz <zxxzyyz2020@gmail.com>
2022-11-01 22:28:30 +0900

    Before AOP

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -16,9 +16,16 @@ public class MemberService {
     }
 
     public Long join(Member member) {
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+        try {
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timsMs = finish - start;
+            System.out.println("join " + timsMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -28,7 +35,14 @@ public class MemberService {
     }
 
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timsMs = finish - start;
+            System.out.println("findMembers " + timsMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId) {
