f92eb182358c09924049bff6ce577d13753e207d
zxxzyyz <zxxzyyz2020@gmail.com>
2022-11-06 17:26:43 +0900

    MVC v2... added MyView

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,20 @@
+package hello.servlet.web.frontcontroller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyView {
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    public void render(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req, res);
+    }
+}
