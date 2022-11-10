0ec7f70065f0773359ecb431450cb3e32b76fe4c
zxxzyyz <zxxzyyz2020@gmail.com>
2022-11-10 11:19:30 +0900

    Validation v4... Split add/edit validation process by adding ItemSaveFrom/ItemUpdateForm class

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,16 +12,16 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "error msg")
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class)
+    //@NotNull(groups = UpdateCheck.class)
     private Long id;
 
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+    //@NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+    //@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    //@Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = SaveCheck.class)
+    //@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    //@Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
---
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV4.java
@@ -0,0 +1,94 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.SaveCheck;
+import hello.itemservice.domain.item.UpdateCheck;
+import hello.itemservice.web.validation.form.ItemSaveForm;
+import hello.itemservice.web.validation.form.ItemUpdateForm;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/validation/v4/items")
+@RequiredArgsConstructor
+public class ValidationItemControllerV4 {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "validation/v4/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v4/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "validation/v4/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(@Validated @ModelAttribute("item") ItemSaveForm form, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        if (form.getPrice() != null && form.getQuantity() != null) {
+            int result = form.getPrice() * form.getQuantity();
+            if (result < 10000) bindingResult.reject("totalPriceMin", new Object[]{10000, result},null);
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v4/addForm";
+        }
+
+        Item item = new Item(form.getItemName(), form.getPrice(), form.getQuantity());
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v4/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v4/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@Validated @ModelAttribute("item") ItemUpdateForm form, BindingResult bindingResult, @PathVariable Long itemId) {
+
+        if (form.getPrice() != null && form.getQuantity() != null) {
+            int result = form.getPrice() * form.getQuantity();
+            if (result < 10000) bindingResult.reject("totalPriceMin", new Object[]{10000, result},null);
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v4/editForm";
+        }
+
+        Item itemParam = new Item(form.getItemName(), form.getPrice(), form.getQuantity());
+        itemRepository.update(itemId, itemParam);
+        return "redirect:/validation/v4/items/{itemId}";
+    }
+
+}
+
