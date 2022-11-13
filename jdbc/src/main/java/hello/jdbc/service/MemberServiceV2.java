a8dba3506b20092838122cdaaf020d981814fd58
zxxzyyz <zxxzyyz2020@gmail.com>
2022-11-13 11:55:45 +0900

    DB... Transaction without Spring

---
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -0,0 +1,66 @@
+package hello.jdbc.service;
+
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 파라미터 연동, 풀을 고려한 종료
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV2 {
+
+    private final DataSource dataSource;
+    private final MemberRepositoryV2 memberRepositoryV2;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Connection con = dataSource.getConnection();
+
+        try {
+            con.setAutoCommit(false);
+
+            businessLogic(con, fromId, toId, money);
+
+            con.commit();
+        } catch (Exception e) {
+            con.rollback();
+            throw new IllegalStateException(e);
+        } finally {
+            release(con);
+        }
+
+    }
+
+    private void businessLogic(Connection con, String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepositoryV2.findById(con, fromId);
+        Member toMember = memberRepositoryV2.findById(con, toId);
+
+        memberRepositoryV2.update(con, fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepositoryV2.update(con, toId, toMember.getMoney() + money);
+    }
+
+    private static void release(Connection con) {
+        if (con != null) {
+            try {
+                con.setAutoCommit(true); //커넥션 풀 고려
+                con.close();
+            } catch (Exception e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
