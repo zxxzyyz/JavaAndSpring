76644f761b0bc81b757a1bf01fe304d82ea44e11
zxxzyyz <zxxzyyz2020@gmail.com>
2022-11-15 11:46:57 +0900

    DB... JPA Entity

--- itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -1,18 +1,29 @@
 package hello.itemservice.domain;
 
 import lombok.Data;
+import lombok.Generated;
+
+import javax.persistence.*;
+import java.lang.annotation.Target;
 
 @Data
+@Entity
+//@Table(name = "item") 객체명이랑 동일시 생략가능
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // 스프링 JPA 사용시 카멜 스타일을 언더스코어 스타일로 자동 변환 해주기 때문에 아래의 name도 생략 가능
+    @Column(name = "item_name", length = 10)
     private String itemName;
+
+    //@Column(name = "price") 필드명이랑 동일시 생략가능
     private Integer price;
     private Integer quantity;
 
-    public Item() {
-    }
+    // JPA는 public/protected 기본 생성자 필수
+    public Item() {}
 
     public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
