038ff16937992b7e54b7da414ac1036952bc94b4
zxxzyyz <zxxzyyz2020@gmail.com>
2022-11-19 10:40:34 +0900

    JDK Dynamic Proxy

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,6 +2,8 @@ package hello.proxy;
 
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.config.v1_proxy.concrete_proxy.ConcreteProxyConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -10,7 +12,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 //@Import({AppV1Config.class, AppV2Config.class})
-@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
+//@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
+//@Import({ConcreteProxyConfig.class, DynamicProxyBasicConfig.class})
+@Import({ConcreteProxyConfig.class, DynamicProxyFilterConfig.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
