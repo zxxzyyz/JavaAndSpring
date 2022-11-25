d5ce57b20561edee9ea7477577461a1aa0668bb9
zxxzyyz <zxxzyyz2020@gmail.com>
2022-11-26 08:44:59 +0900

    JPA Simple App... HTML/CSS

--- Jpa1/src/main/java/hello/Jpa1/repository/OrderRepository.java
+++ Jpa1/src/main/java/hello/Jpa1/repository/OrderRepository.java
@@ -7,6 +7,7 @@ import hello.Jpa1.domain.OrderStatus;
 import hello.Jpa1.domain.QMember;
 import hello.Jpa1.domain.QOrder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
@@ -14,11 +15,15 @@ import javax.persistence.EntityManager;
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
 public class OrderRepository {
 
     private final EntityManager em;
     private final JPAQueryFactory query;
+
+    public OrderRepository(EntityManager em) {
+        this.em = em;
+        this.query = new JPAQueryFactory(em);
+    }
 
     public void save(Order order) {
         em.persist(order);
