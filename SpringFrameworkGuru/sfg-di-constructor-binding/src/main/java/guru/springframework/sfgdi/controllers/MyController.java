25d18b678452501d57d7e9fc65ac2b32c85ecd95
zxxzyyz <zxxzyyz2020@gmail.com>
2023-01-24 09:34:06 +0900

    SFG DI Qualifiers

--- SpringFrameworkGuru/sfg-di-constructor-binding/src/main/java/guru/springframework/sfgdi/controllers/MyController.java
+++ SpringFrameworkGuru/sfg-di-constructor-binding/src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -1,6 +1,5 @@
 package guru.springframework.sfgdi.controllers;
 
-import guru.springframework.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -9,13 +8,9 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MyController {
 
-    private final GreetingService greetingService;
-
-    public MyController(GreetingService greetingService) {
-        this.greetingService = greetingService;
-    }
-
     public String sayHello(){
-        return greetingService.sayGreeting();
+        System.out.println("Hello World!!!");
+
+        return "Hi Folks!";
     }
 }
