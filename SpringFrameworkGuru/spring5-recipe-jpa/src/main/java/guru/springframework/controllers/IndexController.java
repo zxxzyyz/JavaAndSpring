f6a0efcf45d03be760ddcd55d71cb39766314563
zxxzyyz <zxxzyyz2020@gmail.com>
2023-02-02 12:46:54 +0900

    SFG Spring Data Initialization

--- SpringFrameworkGuru/spring5-recipe-jpa/src/main/java/guru/springframework/controllers/IndexController.java
+++ SpringFrameworkGuru/spring5-recipe-jpa/src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,7 +1,13 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 /**
  * Created by jt on 6/1/17.
