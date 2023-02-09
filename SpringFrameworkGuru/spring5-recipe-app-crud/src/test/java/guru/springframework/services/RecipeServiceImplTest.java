66841aa9cd6c46e7a7fc886df82df84e41beb65c
zxxzyyz <zxxzyyz2020@gmail.com>
2023-02-09 21:54:42 +0900

    SFG CRUD view

--- SpringFrameworkGuru/spring5-recipe-app-crud/src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ SpringFrameworkGuru/spring5-recipe-app-crud/src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -84,8 +84,7 @@ public class RecipeServiceImplTest {
         HashSet receipesData = new HashSet();
         receipesData.add(recipe);
 
-        //when(recipeService.getRecipes()).thenReturn(receipesData);
-        when(recipeRepository.findAll()).thenReturn(receipesData);
+        when(recipeService.getRecipes()).thenReturn(receipesData);
 
         Set<Recipe> recipes = recipeService.getRecipes();
 
